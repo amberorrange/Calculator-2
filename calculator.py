622e6f137e2798734d8a357a098b71f23c13bfad
Amber Orrange <orrangeamber@gmail.com>
2021-10-26 19:07:26 -0500

    calc 2

--- calculator.py
+++ calculator.py
@@ -19,60 +19,73 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 #             (...etc.)
 
 
-while True: 
+while True:
+    user_input = input('Enter your equation > ')
+    tokens = user_input.split(' ')
 
-    problem = input("Enter an equation: ")
-    problem = problem.split(" ")
-    op = problem[0]
-
-    if op == "q":
-        print("goodbye")
+    if 'q' in tokens:
+        print('You will exit.')
         break
 
-    elif len(problem) < 2:
-        print("Not enough inputs")
+    elif len(tokens) < 2:
+        print('Not enough inputs.')
         continue
 
-    elif len(problem) == 2:
-        op = problem[0]
-        num1 = problem[1]
-        if not num1.isdigit(): 
-            print("use all digits")
-            continue
-        num1 = int(num1)
+    operator = tokens[0]
+    num1 = tokens[1]
 
-        if op == "square":
-            print(float(square(num1)))
+    if len(tokens) < 3:
+        num2 = '0'
 
-        elif op == "cube": 
-            print(float(cube(num1)))
-        
-    if len(problem) > 2:
-        op = problem[0]
-        num1 = problem[1]
-        num2 = problem[2]
-        if not num2.isdigit() or not num1.isdigit():
-            print("use all digits")
-            continue
-        num1 = int(num1)
-        num2= int(num2)
-    
-        if op == "+": 
-            print(float(add(num1, num2)))
+    else:
+        num2 = tokens[2]
 
-        elif op == "-": 
-            print(float(subtract(num1, num2)))
-        
-        elif op == "*": 
-            print(float(multiply(num1, num2)))
-        
-        elif op == "/": 
-            print(float(divide(num1, num2)))
-        
-        elif op == "mod": 
-            print(float(mod(num1, num2)))
+    if len(tokens) > 3:
+        num3 = tokens[3]
 
+    # A place to store the return value of the math function we call,
+    # to give us one clear place where that result is printed.
+    result = None
 
-    
+    if not num1.isdigit() or not num2.isdigit():
+        print('Those aren\'t numbers!')
+        continue
 
-     
+    # We have to cast each value we pass to an arithmetic function from a
+    # a string into a numeric type. If we use float across the board, all
+    # results will have decimal points, so let's do that for consistency.
+
+    elif operator == '+':
+        result = add(float(num1), float(num2))
+
+    elif operator == '-':
+        result = subtract(float(num1), float(num2))
+
+    elif operator == '*':
+        result = multiply(float(num1), float(num2))
+
+    elif operator == '/':
+        result = divide(float(num1), float(num2))
+
+    elif operator == 'square':
+        result = square(float(num1))
+
+    elif operator == 'cube':
+        result = cube(float(num1))
+
+    elif operator == 'pow':
+        result = power(float(num1), float(num2))
+
+    elif operator == 'mod':
+        result = mod(float(num1), float(num2))
+
+    elif operator == 'x+':
+        result = add_mult(float(num1), float(num2), float(num3))
+
+    elif operator == 'cubes+':
+        result = add_cubes(float(num1), float(num2))
+
+    else:
+        result = 'Please enter an operator followed by two integers.'
+
+    print(result)
